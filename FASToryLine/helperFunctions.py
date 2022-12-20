bf5db1e3589657815bba3ac59716ff5bcf7ecf1c
Mahboob Elahi <46259412+mahboobelahi@users.noreply.github.com>
2022-12-20 22:34:39 +0200

    major updates, manily done for subscription parts

--- FASToryLine/helperFunctions.py
+++ FASToryLine/helperFunctions.py
@@ -5,10 +5,18 @@ from FASToryLine import db
 from FASToryLine import configurations as CONFIG 
 from sqlalchemy import exc
 from FASToryLine import ProductionPolicy as WkC
-#creating data base modles
 
-WS_obj_list = list()
 
+
+#checking machine local IP
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+########DataBase related Helper functions#################
 # create tables in DB
 def createModels():
     db.create_all()
@@ -45,40 +53,7 @@ def insertLineEvents(event_notif,id):
     except exc.SQLAlchemyError as e:
         print(f'[XE] {e}')
 
-#checking machine local IP
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
-
-# formatting data for orders to make processing easy
-def formated_orders(raw_orders):#current_order,
-    """
-    converts the raw orders, from a query result of DB to a formatted and easy to process dictionary orders list
-    :param raw_orders: list of order tuples
-    :param row: contains list of tuples
-    :return: formatted dictionary orders list
-    """
-    formatted_orders = list()#{}
-    for order in raw_orders:
-
-        key = 'Order_'+str(order[0])#'order_specs'  # + str(row[i][0])
-        # order specifications
-        for i in range(order[7]):
-            temp_=list(
-                        (#order[0],
-                        'Order'+str(order[0]),
-                         {"Frame": list(order[1:3])},
-                         {"Screen": list(order[3:5])},
-                         {"Keypad": list(order[5:7])}#{"Quantity": order[7]}
-                         )
-            )
-            formatted_orders.append(temp_ )
-        #print('HF8: Temp Order: ', formatted_orders)
-    return formatted_orders
-
+################## production related###############
 #called by policyBasedToolChange
 def changePen(result):
     try:
@@ -95,7 +70,7 @@ def changePen(result):
             if currentPen == 'NA': #2,9,10,11,12
                 res = requests.post(robServiceURL+f'ChangePen{capability[0]}',json={"destUrl": ""})
                 if res.status_code not in [200,202.0]:   
-                    print(f'[X]')
+                    print(f'[X] Workstation_{result.WorkCellID}')
                     P(res.json())
                 else:
                     print(f'[X] Workstation_{result.WorkCellID} has changed Pencolor to {capability[0]}, ({res.status_code},{res.reason})' )
@@ -118,7 +93,7 @@ def changePen(result):
 def policyBasedToolChanging(id=None,capability=None):
     try:
         if id:
-            WS_obj_list[id-1]. WkSINFO()
+            CONFIG.WS_obj_list[id-1]. WkSINFO()
             changePen( WorkstationInfo.query.get_or_404(id))
             return
         else:
@@ -160,6 +135,7 @@ def updateCapability(policyID ):
     except exc.SQLAlchemyError as e:
         print(f'[XE] {e}')
 
+#fetch orders from next availabe production lot 
 def getAndSetIsFetchOrders():
     try:
         result= Orders.query.filter_by(IsFetched=False).first()
@@ -170,17 +146,18 @@ def getAndSetIsFetchOrders():
     except exc.SQLAlchemyError as e:
         print(f'[XE] {e}')
 
+
+############## Orchestrator will call  for starting servers for FASTory Workstations#################
 def instencateWorkstations():
     #instentiate Workstation servers according to Production policy
-    global  WS_obj_list 
     CONFIG.wrkCellLoc_Port = 2000
     for i in range(1,13):
         #change capabilities for policies
-        if i in [1,2,3,4,5,6,7,8,10,11,12]:
-            continue
+        # if i in [1,2,3,4,5,6,7,8,10,11,12]:
+        #     continue
         # if i !=7 and  i!=10:
         #     continue
-        # if  i!=10:
+        # if  i==3:
         #      continue
         temp_obj=WkC.Workstation(i,CONFIG.wrkCellLoc_Port+i,
                                 CONFIG.robot_make[i-1],CONFIG.robot_type[i-1],
@@ -196,14 +173,43 @@ def instencateWorkstations():
             temp_obj.callWhenDBdestroyed()
         time.sleep(1)
         temp_obj.updateIP()
-        WS_obj_list.append(temp_obj)
+        CONFIG .WS_obj_list.append(temp_obj)
     print(f"AccessOrchestrtor here: http://{get_local_ip()}:1064")
-
     #CNV and Robot events subscriptions
-    if len(S1000Subscriptions.query.all()) == 0:
+    #subscribeToFASToryEvents(CONFIG.WS_obj_list)
+    return ""
+
+################ related to line sub-Unsub-scriptions################
+def subscribeToFASToryEvents(WS_obj_list,Unsub=True):
+    if Unsub:
+        for obj in WS_obj_list:
+                if obj.get_ID() == 1:continue
+                print(f'[X] UnSubscription for Workstation_{obj.get_ID()}' )
+                # if obj.get_ID() == 7:
+                #     print("")
+                for zone_name in range(1, 6):
+                    if zone_name == 5:
+                        continue
+                    obj.CNV_event_Unsubscriptions(zone_name)
+
+                if obj.get_ID() == 1:
+                    obj.ROB_event_Unsubscriptions('PaperLoaded')
+                    obj.ROB_event_Unsubscriptions('PaperUnloaded')
+                elif obj.get_ID() == 7:
+                    
+                    obj.ROB_event_Unsubscriptions('PalletLoaded')
+                    obj.ROB_event_Unsubscriptions('PalletUnloaded')
+                else:
+                    obj.ROB_event_Unsubscriptions('PenChangeStarted')
+                    obj.ROB_event_Unsubscriptions('DrawStartExecution')
+                    obj.ROB_event_Unsubscriptions('PenChangeEnded')
+                    obj.ROB_event_Unsubscriptions('DrawEndExecution')
+    else:
         for obj in WS_obj_list:
             if obj.get_ID() == 1:continue
-            print('\nSubscription for Workstation_%d\n' % obj.get_ID())
+            print(f'[X] Subscription for Workstation_{obj.get_ID()}' )
+            # if obj.get_ID() == 7:
+            #     print("")
             for zone_name in range(1, 6):
                 if zone_name == 5:
                     continue
@@ -214,14 +220,33 @@ def instencateWorkstations():
                 obj.ROB_event_subscriptions('PaperUnloaded')
             elif obj.get_ID() == 7:
                 
-                # obj.ROB_event_subscriptions('PalletLoaded')
+                obj.ROB_event_subscriptions('PalletLoaded')
                 obj.ROB_event_subscriptions('PalletUnloaded')
             else:
-                #obj.ROB_event_subscriptions('PenChangeStarted')
-                #obj.ROB_event_subscriptions('DrawStartExecution')
+                obj.ROB_event_subscriptions('PenChangeStarted')
+                obj.ROB_event_subscriptions('DrawStartExecution')
                 obj.ROB_event_subscriptions('PenChangeEnded')
                 obj.ROB_event_subscriptions('DrawEndExecution')
-    return ""
+
+def checkSubscription(url,body):
+    try:
+        eventId = None
+        # check for event subscriptions
+        r = requests.get(url) 
+        if r.status_code == 404:
+            return (False,eventId) 
+        if (r.json().get("children")): 
+            eventId= [x for x in r.json().get("children").values()][0].get("id")
+            r=requests.get(f"{url}/{eventId}")
+            #check application subscription
+            if (r.json().get("destUrl") == body.get("destUrl")):
+                return (True,eventId)
+        else:
+            return (False,eventId)
+    except requests.exceptions.RequestException as err:
+        print("[X] OOps: Something Else", err) 
+
+################# used if event sub-unSub done from eventsubscriptions table#################
 
 def sendHTTPReqtoS1000(url, Unsubs=False):
     try: 
@@ -234,6 +259,7 @@ def sendHTTPReqtoS1000(url, Unsubs=False):
 
     except requests.exceptions.RequestException as err:
         print("[X-Err] OOps: Something Else", err)
+
 def subEvents(url):
     print( f"[X] {url.getSubsEventURLS[1]},{url.getSubsEventURLS[0]}")
     time.sleep(.2)
